787702772c95cab99a4ada0455422302e7e3b24f
jimmyjimmy23 <jacobkerl@yandex.ru>
2022-02-11 12:20:32 +0300

    Initial commit

---
+++ src/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Java_Stdin_and_Stdout_I {
+    public static class Solution {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+    }
+}
